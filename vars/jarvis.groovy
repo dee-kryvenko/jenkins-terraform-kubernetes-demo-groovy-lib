277552bd8c2d8a08f05b7a4c9f312258b5798ed2
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 02:01:21 -0700

    make hcl compatible

--- vars/jarvis.groovy
+++ vars/jarvis.groovy
@@ -1,6 +1,9 @@
 def call(Closure body) {
-    def jarvis = new Class() {
+    def jarvis = new Object() {
         def version
+        def version(String version) {
+            this.version = version
+        }
     }
     body.setDelegate(jarvis)
     body.setResolveStrategy(Closure.DELEGATE_FIRST)
