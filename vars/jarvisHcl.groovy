f985fd8bae34387301657140c4c2b4dc5490cc36
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 04:28:34 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,10 +1,10 @@
 def call1(type, args) {
     println type
     println args[0]
-    return new Object() {
-        def methodMissing(name, args1) {
-            println name
-            println args1[0]
-        }
+    def fixture = new Object()
+    fixture.metaClass.methodMissing { name, args1 ->
+        println name
+        println args1[0]
     }
+    return fixture
 }
