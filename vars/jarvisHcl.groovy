612049b3d383b03baabd0a80ca7e07c5e5847c6c
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 04:23:40 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,7 +1,7 @@
 def call1(type, args) {
-    this.metaClass.methodMissing { name, args ->
+    this.metaClass.methodMissing { name, args1 ->
         println name
-        println args[0]
+        println args1[0]
     }
     println type
     println args[0]
