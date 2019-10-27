c8b28c310171c0645b7828cc0336702fc50a04f9
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 04:59:29 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,8 +1,9 @@
-def get(String resource, type) {
+def get(context, String resource, type) {
     type = type[0] as String
     def hcl = new Object()
     hcl.metaClass.methodMissing { String name, args ->
         Closure body = args[0]
+        println context.class
         println "${resource}.${type}.${name}"
         println body
     }
