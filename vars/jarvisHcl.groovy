af3d77c43e28ac6359751ec255a858e09b894e59
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 05:34:16 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,12 +1,12 @@
 def get(context, String resource, type) {
     if (type.size() != 1 || !(type[0] instanceof String)) {
-        throw new MissingMethodException(resource, context.class, type)
+        context.originalMethodMissing(resource, type)
     }
     type = type[0] as String
     def hcl = new Object()
     hcl.metaClass.methodMissing { String name, args ->
         if (args.size() != 1 || !(args[0] instanceof Closure)) {
-            throw new MissingMethodException(name, context.class, args)
+        context.originalMethodMissing(name, args)
         }
 
         Closure body = args[0]
