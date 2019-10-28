a78606b1ec2fdb10e4a464cebd0aeff279d2c438
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 18:57:48 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,10 +1,12 @@
+import org.jenkinsci.plugins.workflow.cps
+
 def get(context, String resource, type) {
     if (type.size() != 1 || !(type[0] instanceof String)) {
-        return context."${resource}"(type)
+        return context.steps."${resource}"(type)
     }
     type = type[0] as String
     def hcl = new Object()
-    hcl.metaClass.invokeMissingMethod { String name, args ->
+    hcl.metaClass.methodMissing { String name, args ->
         if (args.size() != 1 || !(args[0] instanceof Closure)) {
             throw new MissingMethodException(name, context.class, args)
         }
