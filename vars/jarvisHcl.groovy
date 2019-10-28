93b7cf43d0d47ca39f63a6d154f73d84faac6cf6
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 19:13:59 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -15,3 +15,38 @@ def get(context, String resource, type) {
     }
     return hcl
 }
+
+def do() {
+    evaluate """
+pipeline {
+  agent {
+    kubernetes {
+      defaultContainer 'jnlp'
+      yaml '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: some-label-value
+spec:
+  containers:
+  - name: maven
+    image: maven:alpine
+    command:
+    - cat
+    tty: true
+'''
+    }
+  }
+  stages {
+    stage('Run maven') {
+      steps {
+        container('maven') {
+          sh 'mvn -version'
+        }
+      }
+    }
+  }
+}
+"""
+}
