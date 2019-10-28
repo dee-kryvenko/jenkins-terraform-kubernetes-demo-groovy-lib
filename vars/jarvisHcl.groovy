6c6f4c0bf53b32f96b3fd0cf35203e843e91d19a
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 18:51:27 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,12 +1,10 @@
-import org.codehaus.groovy.runtime.InvokerHelper
-
 def get(context, String resource, type) {
     if (type.size() != 1 || !(type[0] instanceof String)) {
-        return "${resource}"(type)
+        return context."${resource}"(type)
     }
     type = type[0] as String
     def hcl = new Object()
-    hcl.metaClass.methodMissing { String name, args ->
+    hcl.metaClass.invokeMethod { String name, args ->
         if (args.size() != 1 || !(args[0] instanceof Closure)) {
             throw new MissingMethodException(name, context.class, args)
         }
