0f4080dbbee56c492f645c58e60c5af433be37d3
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 17:20:29 -0700

    debug

--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -3,7 +3,7 @@ def get(context, originalMethodMissing, String resource, type) {
         println originalMethodMissing.class
         // context.metaClass.methodMissing = originalMethodMissing
         // context.methodMissing(resource, type)
-        originalMethodMissing.invokeMethod(context, resource, type)
+        originalMethodMissing.call([resource, type])
         return context
     }
     type = type[0] as String
