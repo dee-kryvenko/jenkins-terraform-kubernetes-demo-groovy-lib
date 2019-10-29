f2f9663cf2e4ac5ce2e37b0bdc80c0b79875bea7
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-29 16:53:38 -0700

    debug

--- src/jarvis/jenkins/lib/Hcl.groovy
+++ src/jarvis/jenkins/lib/Hcl.groovy
@@ -52,7 +52,7 @@ class Hcl implements Serializable {
     private Class<AbstractConfig> getConfigClass(String resource, String type) {
         new ClassGraph().enableAllInfo().scan().withCloseable { scanResult ->
             context.steps.echo Config.class.getName()
-            context.steps.echo scanResult.getAllClasses().size().toString()
+            context.steps.echo scanResult.getClassesWithAnnotation(Config.class.getName()).size().toString()
             scanResult.getClassesWithAnnotation(Config.class.getName()).find() {
                 context.steps.echo it.toString()
                 AnnotationParameterValueList config = it.getAnnotationInfo(Config.class.getName()).getParameterValues()
