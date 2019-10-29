f84554b3c0fa73c6013ce25e3118254dfac47ff1
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-29 15:14:10 -0700

    debug

--- src/jarvis/jenkins/lib/config/Config.groovy
+++ src/jarvis/jenkins/lib/config/Config.groovy
@@ -1,5 +1,9 @@
 package jarvis.jenkins.lib.config
 
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
+@Retention(RetentionPolicy.RUNTIME)
 @interface Config {
     String resource()
     String type()
