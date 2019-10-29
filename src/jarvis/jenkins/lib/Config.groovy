a4a288b29b75f60a89a0b3f271ac3aabea8427dd
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-29 13:28:23 -0700

    Add simple config implementation

---
+++ src/jarvis/jenkins/lib/Config.groovy
@@ -0,0 +1,37 @@
+package jarvis.jenkins.lib
+
+import com.cloudbees.groovy.cps.NonCPS
+
+class Config implements Serializable {
+    private static class ConfigHolder implements Serializable {
+        static Config INSTANCE
+    }
+
+    @NonCPS
+    static Config it() {
+        return ConfigHolder.INSTANCE
+    }
+
+    @NonCPS
+    static void init(context) {
+        if (it() == null) {
+            ConfigHolder.INSTANCE = new Config(context)
+        }
+    }
+
+    private def context
+    private Map<String, Closure> hcl = [:]
+
+    Config(context) {
+        this.context = context
+    }
+
+    void add(String resource, String type, String name, Closure body) {
+        hcl.put(String.join(".", [resource, type, name], body))
+    }
+
+    @Override
+    String toString() {
+        this.hcl.keySet().join(", ")
+    }
+}
--- vars/jarvisHcl.groovy
+++ vars/jarvisHcl.groovy
@@ -1,22 +1,30 @@
+import jarvis.jenkins.lib.Config
+
 def get(context, String resource, type) {
+    Config.init(this)
+
     if (type.size() != 1 || !(type[0] instanceof String)) {
         return context.steps.invokeMethod(resource, type)
     }
+
     type = type[0] as String
+
     def hcl = new Object()
     hcl.metaClass.methodMissing { String name, args ->
         if (args.size() != 1 || !(args[0] instanceof Closure)) {
-            throw new MissingMethodException(name, context.class, args)
+            return context.steps.invokeMethod(name, args)
         }
 
         Closure body = args[0]
-        println "${resource}.${type}.${name}"
-        println body
+
+        Config.it().add(resource, type, name, body)
     }
+
     return hcl
 }
 
 def done(steps) {
+    def result = Config.it().toString()
     evaluate """
 pipeline {
   agent {
@@ -43,6 +51,7 @@ spec:
       steps {
         container('maven') {
           sh 'mvn -version'
+          echo "${result}"
         }
       }
     }
