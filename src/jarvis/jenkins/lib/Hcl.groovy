818f4a7765ffcf2323bd473c033641a1faf7c060
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-30 15:44:37 -0700

    debug

--- src/jarvis/jenkins/lib/Hcl.groovy
+++ src/jarvis/jenkins/lib/Hcl.groovy
@@ -29,11 +29,13 @@ class Hcl implements Serializable {
 
     void add(String resource, String type, String name, Closure body) {
         if (!hcl.containsKey(resource)) {
+            context.steps.echo "init ${resource}"
             hcl.put(resource, [:])
         }
 
         Map<String, Map<String, AbstractConfig>> types = hcl[resource]
         if (!types.containsKey(type)) {
+            context.steps.echo "init ${type}"
             types.put(type, [:])
         }
 
@@ -42,10 +44,12 @@ class Hcl implements Serializable {
             throw new RuntimeException("${resource}.${type}.${name} already defined")
         }
 
+        context.steps.echo "parsing ${resource}.${type}"
         AbstractConfig config = findClass('config', resource, type)
         body.setDelegate(config)
         body.setResolveStrategy(Closure.DELEGATE_FIRST)
         hcl.each { key, value ->
+            context.steps.echo "set ${key} to ${resource}.${type}"
             body.setProperty(key, value)
         }
         body.call()
