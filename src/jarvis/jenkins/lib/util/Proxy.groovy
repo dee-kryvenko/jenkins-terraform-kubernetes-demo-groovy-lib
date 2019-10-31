d4953744d7d8161ec0839816d9ad7e24ef9cdc9a
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-30 20:49:47 -0700

    debug

--- src/jarvis/jenkins/lib/util/Proxy.groovy
+++ src/jarvis/jenkins/lib/util/Proxy.groovy
@@ -3,7 +3,7 @@ package jarvis.jenkins.lib.util
 class Proxy extends groovy.util.Proxy {
     Object getProperty(String propertyName) {
         if (this.hasProperty(propertyName)) {
-            return this.getProperty(propertyName)
+            return super.getProperty(propertyName)
         } else {
             this.getAdaptee().getProperty(propertyName)
         }
