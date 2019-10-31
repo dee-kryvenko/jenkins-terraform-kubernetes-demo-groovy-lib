430768dbeb77d6afe89dab85585be3b89f28cee1
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-30 17:47:53 -0700

    debug

--- src/jarvis/jenkins/lib/Hcl.groovy
+++ src/jarvis/jenkins/lib/Hcl.groovy
@@ -45,7 +45,7 @@ class Hcl implements Serializable {
         AbstractConfig config = findClass('config', resource, type)
 //        Closure bodyClone = body.rehydrate(config, hcl, hcl)
         body.setDelegate(config)
-        body.setResolveStrategy(Closure.DELEGATE_FIRST)
+        body.setResolveStrategy(Closure.DELEGATE_ONLY)
         hcl.each { key, value ->
             context.steps.echo "${resource}.${type}.${name} << ${key}: ${value}"
             body.setProperty(key, value)
