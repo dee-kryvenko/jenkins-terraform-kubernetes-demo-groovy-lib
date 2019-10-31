a623327f5d65dab89067efd22c8de8e9b7b4683d
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-31 03:13:17 -0700

    debug

--- src/jarvis/jenkins/lib/Pipeline.groovy
+++ src/jarvis/jenkins/lib/Pipeline.groovy
@@ -19,9 +19,9 @@ class Pipeline implements Serializable {
 pipeline {
   agent none
   stages {
-<% stages.each { %>
+<%- stages.each { -%>
 <% out.print it.readLines().collect { line -> "    ${line}" }.join('\\n') %>
-<% } %>
+<%- } -%>
   }
 }
 '''
@@ -30,9 +30,9 @@ pipeline {
 stage("${stageName}") {
   agent ${agent}
   steps {
-<% steps.each { %>
+<%- steps.each { -%>
 <% out.print it.readLines().collect { line -> "    ${line}" }.join('\\n') %>
-<% } %>
+<%- } -%>
   }
 }
 '''
@@ -44,9 +44,9 @@ stage("${stageName}") {
       kind: Pod
       spec:
         containers:
-<% containers.each { %>
+<%- containers.each { -%>
   <% out.print it.readLines().collect { line -> "        ${line}" }.join('\\n') %>
-<% } %>
+<%- } -%>
 '''
 
         List<String> testingContainers = []
